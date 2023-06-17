947a6cd4bcc44640f8b1147c4a8cc8f6eb47b6f1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-16 22:26:22 -0300

    Fluxograma

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+                 Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("---- Fluxograma Da Engenharia ----\n".ToUpper());
+            Console.ResetColor();
+
+            string movendo, deveria;
+
+            Console.Write("O objeto está se movendo? (S/N)\n");
+            movendo = Console.ReadLine()!.ToUpper();
+
+            if (movendo == "S")
+
+            {
+                Console.Write("Deveria se mover? (S/N) \n");
+                deveria = Console.ReadLine()!.ToUpper();
+                
+                {
+                    if (deveria == "S")
+                    {
+                        Console.WriteLine("Ótimo");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Use silvertape");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.Write("Deveria se mover? (S/N) \n");
+                deveria = Console.ReadLine()!.ToUpper();
+
+                {
+                    if (deveria == "S")
+                    {
+                        Console.WriteLine("Use WD40");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ótimo");
+                    }
+                }
+            }
+        }
+    }
